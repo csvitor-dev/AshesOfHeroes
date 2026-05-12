27d8fabdb0fea57c08b58986dac0f5754d37a03f
csvitor-dev <csvitor.dev@gmail.com>
2026-05-12 16:41:15 -0300

    feat: handler to reading file

---
+++ src/utils/filesystem.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from os import path
+
+
+def read_file(filename: str) -> str:
+    abs_path = path.abspath(filename)
+    target_path = Path(abs_path)
+
+    if target_path.exists() is False:
+        raise FileExistsError
+    return target_path.read_text()
+
+
+def read_vertex_shader(scope: str, filename: str) -> str:
+    return read_file(f"shaders/{scope}/{filename}_vertex.glsl")
+
+
+def read_fragment_shader(scope: str, filename: str) -> str:
+    return read_file(f"shaders/{scope}/{filename}_fragment.glsl")
