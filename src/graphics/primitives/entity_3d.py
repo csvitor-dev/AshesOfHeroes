71a512a519b3d841933cc78d4df0b42e9a33b598
csvitor-dev <csvitor.dev@gmail.com>
2026-05-19 21:14:13 -0300

    fix: use GLM to Linear Algebra transformations in Entity3D

--- src/graphics/primitives/entity_3d.py
+++ src/graphics/primitives/entity_3d.py
@@ -1,5 +1,6 @@
 import ctypes
 import numpy as np
+from pyglm import glm
 from OpenGL.GL import *
 
 
@@ -10,11 +11,10 @@ class Entity3D:
         self._ebo: int = 0
         self._index_count: int = 0
 
-        self.position = np.array([0.0, 0.0, 0.0], dtype=np.float32)
-        self.rotation = np.array(
-            [0.0, 0.0, 0.0], dtype=np.float32)
-        self.scale = np.array([1.0, 1.0, 1.0], dtype=np.float32)
-        self.color = np.array([1.0, 1.0, 1.0, 1.0], dtype=np.float32)
+        self.position = glm.vec3(0.0, 0.0, 0.0)
+        self.rotation = glm.vec3(0.0, 0.0, 0.0)
+        self.scale = glm.vec3(1.0, 1.0, 1.0)
+        self.color = glm.vec4(1.0, 1.0, 1.0, 1.0)
 
     def _upload(self, vertices: np.ndarray, indices: np.ndarray):
         self._index_count = len(indices)
@@ -33,7 +33,7 @@ class Entity3D:
         glBufferData(GL_ELEMENT_ARRAY_BUFFER,
                      indices.nbytes, indices, GL_STATIC_DRAW)
 
-        stride = 6 * 4  # 6 floats × 4 bytes
+        stride = 6 * 4
 
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,
                               stride, ctypes.c_void_p(0))
@@ -45,8 +45,16 @@ class Entity3D:
 
         glBindVertexArray(0)
 
-    def model_matrix(self) -> np.ndarray:
-        return mat4_trs(self.position, self.rotation, self.scale)
+    def model_matrix(self) -> glm.mat4x4:
+        model = glm.translate(glm.mat4(1.0), self.position)
+        model = glm.rotateX(model, glm.radians(
+            self.rotation.x)) if self.rotation.x != 0.0 else M
+        model = glm.rotateY(model, glm.radians(
+            self.rotation.y)) if self.rotation.y != 0.0 else M
+        model = glm.rotateZ(model, glm.radians(
+            self.rotation.z)) if self.rotation.z != 0.0 else M
+        model = glm.scale(model, self.scale)
+        return model
 
     def draw(self, shader_program: int):
         loc_model = glGetUniformLocation(shader_program, "u_model")
